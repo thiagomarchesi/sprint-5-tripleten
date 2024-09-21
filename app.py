4207e33cc9bc06a4c4dda2fb07589c9b26d60914
thiagomarchesi <181411010+thiagomarchesi@users.noreply.github.com>
2024-09-21 16:46:29 -0300

    Update app.py

--- app.py
+++ app.py
@@ -3,3 +3,9 @@ import streamlit as st
 st.header('Jogando uma moeda')
 
 st.write('Ainda não é um aplicativo funcional. Em construção.')
+
+$ ls -a
+.git .gitignore .streamlit app.py README.md requirements.txt
+
+$ git commit -am "initial commit"
+$ git push
